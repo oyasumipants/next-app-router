dc11655998ab5b35e2b7f59abefd615054db579c
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-07-29 11:28:24 +0900

    feat: layout.tsx

---
+++ src/app/task/layout.tsx
@@ -0,0 +1,14 @@
+const layout = ({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+  return (
+    <div className='bg-blue-300 p4'>
+        <div>タスクレイアウト</div>
+        {children}
+    </div>
+  )
+}
+
+export default layout

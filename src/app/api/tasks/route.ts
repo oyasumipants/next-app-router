1468eb5677573074e89b57108cefa6a2d9f85a66
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-08-01 12:15:14 +0900

    fix: route handler

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+
+export interface Task {
+    id: number;
+    name: string;
+}
+
+const tasks: Task[] = [
+    { id: 1, name: 'Task 1' },
+    { id: 2, name: 'Task 2' },
+    { id: 3, name: 'Task 3' },
+];
+
+export const GET = async () => {
+    return NextResponse.json( {tasks}, {
+        status: 200
+    } );
+}
+
+export const dynamic = 'force-dynamic';

f254e2d84cd2c0ac357224dbd0f949e4acc295bb
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-13 04:19:32 +0900

    feat: server action hook

--- src/actions/serverActions.ts
+++ src/actions/serverActions.ts
@@ -1,8 +1,14 @@
 'use server';
 
-export const createTask = async (taskId: number, formData: FormData) => {
+interface FormState {
+    error: string;
+}
+
+export const createTask = async (taskId: number, state: FormState, formData: FormData) => {
     // DB にタスクを作成.
     console.log('タスクを作成しました');
     console.log(formData.get("name"));
     console.log(taskId);
+    state.error = 'エラーが発生しました';
+    return state;
 }
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,15 +1,35 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
+
 import { createTask } from '@/actions/serverActions'
 
 const ServerActionsPage = () => {
     const taskId = 1;   
     const createTaskWithTaskId = createTask.bind(null, taskId);
-  return (
-    <div>
-        <form action={createTaskWithTaskId}>
-            <input type="text" id='name' name='name' className='bg-gray-200'/>
-            <button type='submit' className='bg-gray-400 ml-2 px-2'>
+    const initialState = { error: ''}
+    const [state, formAction] = useFormState(createTaskWithTaskId, initialState);
+
+    const SubmitButton = () => {
+        const { pending } = useFormStatus();
+        return (
+            <button
+             type='submit'
+              className='bg-gray-400 ml-2 px-2 disabled:bg-gray-300'
+             disabled={pending}
+            >
                 送信
             </button>
+        );
+    };
+  return (
+    <div>
+        <form action={formAction}>
+            <input type="text" id='name' name='name' className='bg-gray-200'/>
+            <SubmitButton />
+            <div>
+                {state.error}  
+            </div>
         </form>
     </div>
   );

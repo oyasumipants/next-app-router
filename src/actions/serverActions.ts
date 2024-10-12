b039be5ecbdd549323e7a674e5e8c8cb201f7a65
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-13 04:08:43 +0900

    feat: add server actions

--- src/actions/serverActions.ts
+++ src/actions/serverActions.ts
@@ -1,7 +1,8 @@
 'use server';
 
-export const createTask = async () => {
+export const createTask = async (taskId: number, formData: FormData) => {
     // DB にタスクを作成.
     console.log('タスクを作成しました');
-
+    console.log(formData.get("name"));
+    console.log(taskId);
 }
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,9 +1,11 @@
 import { createTask } from '@/actions/serverActions'
 
 const ServerActionsPage = () => {
+    const taskId = 1;   
+    const createTaskWithTaskId = createTask.bind(null, taskId);
   return (
     <div>
-        <form action={createTask}>
+        <form action={createTaskWithTaskId}>
             <input type="text" id='name' name='name' className='bg-gray-200'/>
             <button type='submit' className='bg-gray-400 ml-2 px-2'>
                 送信

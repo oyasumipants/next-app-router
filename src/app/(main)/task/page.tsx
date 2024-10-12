8a76468d11fba22aabe93eceaaa61072ef6efc29
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-13 00:00:05 +0900

    feat: add cache

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -3,6 +3,7 @@ import { Task } from "@/app/api/tasks/route";
 const getTasks = async () => {
   const response = await fetch('http://localhost:3000/api/tasks', {
     method: 'GET',
+    cache: 'no-store',
   });
   return await response.json();
 }

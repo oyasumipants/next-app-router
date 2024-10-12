147d87ede0e9095220143bf774e83fbcbf90db20
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-12 23:57:37 +0900

    feat: add task api

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -1,6 +1,23 @@
-const TaskPage = () => {
+import { Task } from "@/app/api/tasks/route";
+
+const getTasks = async () => {
+  const response = await fetch('http://localhost:3000/api/tasks', {
+    method: 'GET',
+  });
+  return await response.json();
+}
+
+const TaskPage = async () => {
+  const tasks = (await getTasks()).tasks as Task[];
   return (
-    <div>TaskPage</div>
+    <div>
+      <div>TaskPage</div>
+      <div>
+        {tasks.map(task => (
+          <div key={task.id}>{task.name}</div>
+        ))}
+      </div>
+    </div>  
   )
 }
 
